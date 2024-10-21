9abd67fc06519f5d11fa5361a2f44961cdbf0209
m8 <umusasadik@gmail.com>
2024-10-21 09:12:05 +0000

    Add machine details

--- runners/_runner.sh
+++ runners/_runner.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
 
 function log_basics {
-    echo "===================="
+    echo "++++++++++++++++++++"
     echo "Time: $(date)"
-    echo "===================="
     echo "APP_DIR: $APP_DIR"
-    echo "SCRIPT_DIR: $SCRIPT_DIR"
-    
+    echo "SCRIPT_DIR: $SCRIPT_DIR"   
     echo "Benchmark: $BENCHMARK"
     echo "Threads: $THREADS"
-    echo "Numactl:" $(numactl --show)
+
+    get_machine_details
+
+    echo "++++++++++++++++++++"
+}
+
+function get_machine_details {
+    echo "----------------"
+    echo "Machine details:"
+    echo "----------------"
+    echo "Host: $(hostname)"
+    echo "CPU: $(lscpu | grep 'Model name' | cut -d':' -f2 | xargs)"
+    echo "Cores: $(lscpu | grep 'CPU(s):' | awk '{print $2}')"
+    echo "Kernel: $(uname -r)"
+    echo "Memory: $(free -h | grep Mem | awk '{print $2}')"
+    echo "Performance governor: $(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)"
+    echo "Scaling min frequency: $(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq)"
+    echo "Scaling max frequency: $(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq)"
+    echo "Transparent Huge Pages: $(cat /sys/kernel/mm/transparent_hugepage/enabled)"
+    echo "Numa hardware: $(numactl --hardware)"
     echo "Prefetcher at core 0:" $(sudo rdmsr 0x1a4)
-    echo "===================="
+    echo ""
 }
 
 function init_bench {
