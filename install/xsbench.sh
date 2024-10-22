0def55e170f91bb80835129d3b39cd75568d981d
m8 <umusasadik@gmail.com>
2024-10-22 09:10:31 +0000

    Add metis and xsbench installers

---
+++ install/xsbench.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Source: https://github.com/ANL-CESAR/XSBench.git
+# Description: Monte Carlo neutron transport simulation code
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+SETUP_DIR=$SCRIPT_DIR/../app_dir/xsbench
+
+if [ "$#" -eq 1 ]; then
+    SETUP_DIR=$1
+fi
+
+mkdir -p $SETUP_DIR
+
+pushd $SETUP_DIR
+git clone https://github.com/ANL-CESAR/XSBench.git .
+cd openmp-threading
+make -j
+popd
+
+# ===========
