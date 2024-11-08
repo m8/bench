54a86d4ae61c860393804db9d78b3a631ad36ddc
m8 <umusasadik@gmail.com>
2024-11-08 16:50:23 +0000

    Add splash trampoline

---
+++ runners/splash2x.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# This is trampoline for splash2 they're using parsec
+
+# == Do not edit ==
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+source $SCRIPT_DIR/parsec.sh
+# == ==
