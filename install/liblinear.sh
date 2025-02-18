f6154d9b59328ef144e7d493e3eba269e0abddef
m8 <umusasadik@gmail.com>
2025-02-18 10:56:26 +0000

    update liblinear path

--- install/liblinear.sh
+++ install/liblinear.sh
@@ -2,7 +2,7 @@
 export SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 SETUP_DIR="../app_dir/liblinear"
-DATASET_DIR="../datasets/liblinear"
+DATASET_DIR="/scratch/musa/datasets/liblinear"
 
 if [ "$#" -eq 1 ]; then
     SETUP_DIR=$1
@@ -13,7 +13,7 @@ mkdir -p $DATASET_DIR
 
 # ====
 pushd $SETUP_DIR
-wget https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/multicore-liblinear/liblinear-multicore-2.47.zip
+wget https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/multicore-liblinear/liblinear-multicore-2.48.zip
 unzip liblinear-multicore-2.47.zip 
 mv liblinear-multicore-2.47/* .
 
