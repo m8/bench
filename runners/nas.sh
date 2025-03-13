0a6b97a8821af1445d77bfaa7b30b23d718e9e94
m8 <umusasadik@gmail.com>
2025-03-13 16:43:36 +0000

    updating nas benchmark

--- runners/nas.sh
+++ runners/nas.sh
@@ -10,8 +10,8 @@ RES_DIR=$SCRIPT_DIR/../results/
 DATASET_DIR=$SCRIPT_DIR/../datasets/
 source $SCRIPT_DIR/_runner.sh
 
-APP_DIR=$APP_DIR/${BENCHMARK}
-RES_DIR=$RES_DIR/${BENCHMARK}
+_APP_DIR=$APP_DIR/${BENCHMARK}
+_RES_DIR=$RES_DIR/${BENCHMARK}
 DATASET_DIR=$DATASET_DIR/${BENCHMARK}
 SUFFIX=""
 PRE_CMD=""
@@ -19,12 +19,22 @@ PRE_CMD=""
 
 runner_init_bench
 
-function nas_ft {
-    pushd $APP_DIR
-    echo "Starting benchmark..." > $RES_DIR/${BENCHMARK}${SUFFIX}.log
-    runner_log_basics >> $RES_DIR/${BENCHMARK}${SUFFIX}.log
-    $PRE_CMD taskset -c 0-$(echo "$THREADS - 1" | bc) ./NPB/NPB3.4-OMP/bin/ft.W.x >> $RES_DIR/${BENCHMARK}${SUFFIX}.log
-    $PRE_CMD taskset -c 0-$(echo "$THREADS - 1" | bc) ./NPB/NPB3.4-OMP/bin/ft.A.x >> $RES_DIR/${BENCHMARK}${SUFFIX}.log
-    $PRE_CMD taskset -c 0-$(echo "$THREADS - 1" | bc) ./NPB/NPB3.4-OMP/bin/ft.D.x >> $RES_DIR/${BENCHMARK}${SUFFIX}.log
+# $1: pass: ft bt lu cg mg sp
+# $2: workloads: A B C D E
+function nas_run {
+    __benchmark=$1
+    __workload=$2
+
+    pushd $_APP_DIR
+    pwd
+    echo "Starting benchmark..." > $_RES_DIR/${BENCHMARK}${SUFFIX}.log
+    echo "Pre command: $PRE_CMD" >> $_RES_DIR/${BENCHMARK}${SUFFIX}.log
+    runner_log_basics >> $_RES_DIR/${BENCHMARK}${SUFFIX}.log
+    $PRE_CMD /usr/bin/time -v taskset -c 0-$(echo "$THREADS - 1" | bc) \
+        ${_APP_DIR}/NPB/NPB3.4-OMP/bin/$__benchmark.$__workload.x >> $_RES_DIR/${BENCHMARK}${SUFFIX}.log 2>&1
     popd
+}
+
+function nas_get_result_file {
+    echo $_RES_DIR/${BENCHMARK}${SUFFIX}.log
 }
